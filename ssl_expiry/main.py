21f165b395a150a0dfa7968fbf3ab534116abc13
Nitesh8011 <nitesh11041999@gmail.com>
2024-11-05 23:16:17 +0530

    ssl expiry jenkins pipeline

--- ssl_expiry/main.py
+++ ssl_expiry/main.py
@@ -2,23 +2,29 @@ from ssl_expire import get_ssl_info
 from mail_script import send_email
 import os
 
+# Retrieve environment variables
 hostnames = os.environ.get('HOSTNAMES')
 
-for hostname in hostnames:
-    ssl_info = get_ssl_info(hostname)
-    if ssl_info:
-        print(f"Hostname: {ssl_info['hostname']}")
-        print(f"Certificate Expiration Date: {ssl_info['expiration_date']}")
-        print(f"Remaining Days: {ssl_info['remaining_days']} days")
+if not hostnames:
+    print("Error: HOSTNAMES environment variables are not set.")
+else:
+    # Split hostnames by commas and clean up whitespace
+    hostname_list = [hostname.strip() for hostname in hostnames.split(',') if hostname.strip()]
 
-        mail_condition = ssl_info['remaining_days']
+    for hostname in hostname_list:
+        ssl_info = get_ssl_info(hostname)
+        if ssl_info:
+            print(f"Hostname: {ssl_info['hostname']}")
+            print(f"Certificate Expiration Date: {ssl_info['expiration_date']}")
+            print(f"Remaining Days: {ssl_info['remaining_days']} days")
 
-        if mail_condition < 10:
-            subject = f"SSL Certificate Expiration Warning for {ssl_info['hostname']}"
-            message = (f"\n\nWarning: \nThe SSL certificate for {ssl_info['hostname']} "
-                       f"\nexpires in {mail_condition} days!")
-            to_email = os.environ.get('TO_EMAIL')
-
-            send_email(subject, message, to_email)
-    else:
-        print(f"Could not retrieve SSL info for {hostname}")
+            # Send an email if certificate expiration is within 10 days
+            if ssl_info['remaining_days'] < 10:
+                subject = f"SSL Certificate Expiration Warning for {ssl_info['hostname']}"
+                message = (f"\n\nWarning: \nThe SSL certificate for {ssl_info['hostname']} "
+                           f"\nexpires in {ssl_info['remaining_days']} days!")
+                
+                to_email = os.environ.get('TO_EMAIL')
+                send_email(subject, message, to_email)
+        else:
+            print(f"Could not retrieve SSL info for {hostname}")
